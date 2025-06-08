c977acef5da68d582ac46bca4e2d0ccdeb88575a
404Shihab <shihabbhuiyan500@gmail.com>
2025-06-09 00:34:31 +0600

    version 8.1

---
+++ Frames/deleteQ.java
@@ -0,0 +1,197 @@
+package Frames;
+
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class deleteQ extends JFrame {
+
+    JLabel namelbl, deleteQLabel, qLabel, optionALabel, optionBLabel, optionCLabel, optionDLabel;
+    JTextField qField, optionAField, optionBField, optionCField, optionDField;
+    JComboBox combo;
+    JButton logOutButton, backButton, addQButton, upQButton, delQButton, seeResButton, showStuDeatilsButton, delButton;
+    JPanel panel, topPanel, sidePanel;
+    ImageIcon icon, adminIcon, deleteQIcon;
+
+    public deleteQ() {
+        super("Delete Questions");
+        this.setSize(950, 600);
+        this.setLocationRelativeTo(null);
+
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBackground(Color.WHITE);
+
+        icon = new ImageIcon(getClass().getResource("../Images/logo.png"));
+        this.setIconImage(icon.getImage());
+
+        // Admin icon
+        adminIcon = new ImageIcon(getClass().getResource("../Images/adminIcon.png"));
+        Image scaledImageRlogo = adminIcon.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+        ImageIcon scaledImageRlogoFinal = new ImageIcon(scaledImageRlogo);
+        JLabel adminLbl = new JLabel(scaledImageRlogoFinal);
+        adminLbl.setBounds(190, 7, 40, 40);
+        panel.add(adminLbl);
+
+        // Back button
+        backButton = new JButton("Back");
+        backButton.setBounds(700, 7, 100, 30);
+        backButton.setForeground(Color.BLUE);
+        backButton.setBackground(Color.WHITE);
+        backButton.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(backButton);
+
+        // Log Out button
+        logOutButton = new JButton("Log Out");
+        logOutButton.setBounds(805, 7, 100, 30);
+        logOutButton.setForeground(Color.BLUE);
+        logOutButton.setBackground(Color.WHITE);
+        logOutButton.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(logOutButton);
+
+        // Top panel
+        topPanel = new JPanel();
+        topPanel.setLayout(null);
+        topPanel.setBounds(185, 0, 950, 50);
+        topPanel.setBackground(new Color(29, 29, 54));
+        panel.add(topPanel);
+
+        namelbl = new JLabel("ADMIN DASHBOARD");
+        namelbl.setBounds(50, 6, 200, 40);
+        namelbl.setFont(new Font("Arial", Font.BOLD, 16));
+        namelbl.setForeground(Color.WHITE);
+        topPanel.add(namelbl);
+
+        // Side panel
+        sidePanel = new JPanel();
+        sidePanel.setLayout(null);
+        sidePanel.setBounds(0, 0, 185, 600);
+        sidePanel.setBackground(Color.BLUE);
+        panel.add(sidePanel);
+
+        // Sidebar buttons
+        addQButton = new JButton("Add Question");
+        addQButton.setBounds(0, 15, 185, 25);
+        addQButton.setForeground(Color.WHITE);
+        addQButton.setBackground(Color.BLUE);
+        addQButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        sidePanel.add(addQButton);
+
+        upQButton = new JButton("Update Question");
+        upQButton.setBounds(0, 75, 185, 25);
+        upQButton.setForeground(Color.WHITE);
+        upQButton.setBackground(Color.BLUE);
+        upQButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        sidePanel.add(upQButton);
+
+        delQButton = new JButton("Delete Question");
+        delQButton.setBounds(0, 135, 185, 25);
+        delQButton.setForeground(Color.BLUE);
+        delQButton.setBackground(Color.WHITE);
+        delQButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        sidePanel.add(delQButton);
+
+        seeResButton = new JButton("See Result");
+        seeResButton.setBounds(0, 195, 185, 25);
+        seeResButton.setForeground(Color.WHITE);
+        seeResButton.setBackground(Color.BLUE);
+        seeResButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        sidePanel.add(seeResButton);
+
+        showStuDeatilsButton = new JButton("Show Details");
+        showStuDeatilsButton.setBounds(0, 255, 185, 25);
+        showStuDeatilsButton.setForeground(Color.WHITE);
+        showStuDeatilsButton.setBackground(Color.BLUE);
+        showStuDeatilsButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        sidePanel.add(showStuDeatilsButton);
+
+        // Mid part - delete icon
+        deleteQIcon = new ImageIcon(getClass().getResource("../Images/deleteQ.png"));
+        Image scaledDeleteQImage = deleteQIcon.getImage().getScaledInstance(140,140, Image.SCALE_SMOOTH);
+        ImageIcon scaledDeleteQIconFinal = new ImageIcon(scaledDeleteQImage);
+        JLabel deleteQJLabel = new JLabel(scaledDeleteQIconFinal);
+        deleteQJLabel.setBounds(450, 60, 140, 140);
+        panel.add(deleteQJLabel);
+
+        // Delete Question text
+        deleteQLabel = new JLabel("Delete Question");
+        deleteQLabel.setBounds(430, 185, 200, 50);
+        deleteQLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        deleteQLabel.setForeground(new Color(197, 68, 68));
+        panel.add(deleteQLabel);
+
+        // Question Label and Field
+        qLabel = new JLabel("Question:");
+        qLabel.setBounds(200, 220, 100, 50);
+        qLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        panel.add(qLabel);
+
+        qField = new JTextField();
+        qField.setBounds(200, 260, 500, 45);
+        qField.setFont(new Font("Arial", Font.BOLD, 15));
+        panel.add(qField);
+
+        // Combo Box for department
+        String[] items = {"CSE", "CS", "CSSE", "SE", "CIS"};
+        combo = new JComboBox<>(items);
+        combo.setBounds(750, 260, 100, 30);
+        combo.setFont(new Font("Arial", Font.BOLD, 14));
+        combo.setBackground(Color.WHITE);
+        combo.setForeground(Color.BLUE);
+        panel.add(combo);
+
+        // Option A
+        optionALabel = new JLabel("Option A:");
+        optionALabel.setBounds(200, 320, 100, 30);
+        optionALabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(optionALabel);
+
+        optionAField = new JTextField();
+        optionAField.setBounds(300, 320, 400, 30);
+        optionAField.setFont(new Font("Arial", Font.PLAIN, 15));
+        panel.add(optionAField);
+
+        // Option B
+        optionBLabel = new JLabel("Option B:");
+        optionBLabel.setBounds(200, 360, 100, 30);
+        optionBLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(optionBLabel);
+
+        optionBField = new JTextField();
+        optionBField.setBounds(300, 360, 400, 30);
+        optionBField.setFont(new Font("Arial", Font.PLAIN, 15));
+        panel.add(optionBField);
+
+        // Option C
+        optionCLabel = new JLabel("Option C:");
+        optionCLabel.setBounds(200, 400, 100, 30);
+        optionCLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(optionCLabel);
+
+        optionCField = new JTextField();
+        optionCField.setBounds(300, 400, 400, 30);
+        optionCField.setFont(new Font("Arial", Font.PLAIN, 15));
+        panel.add(optionCField);
+
+        // Option D
+        optionDLabel = new JLabel("Option D:");
+        optionDLabel.setBounds(200, 440, 100, 30);
+        optionDLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(optionDLabel);
+
+        optionDField = new JTextField();
+        optionDField.setBounds(300, 440, 400, 30);
+        optionDField.setFont(new Font("Arial", Font.PLAIN, 15));
+        panel.add(optionDField);
+
+        // Delete button
+        delButton = new JButton("Delete");
+        delButton.setBounds(750, 440, 100, 30);
+        delButton.setForeground(Color.RED);
+        delButton.setBackground(Color.WHITE);
+        delButton.setFont(new Font("Arial", Font.BOLD, 16));
+        panel.add(delButton);
+
+        this.add(panel);
+    }
+}
--- Main.java
+++ Main.java
@@ -11,14 +11,14 @@ public class Main{
        HomePage h1=new HomePage("Syed Shahriar Mustafa");
        AdminPage a1= new AdminPage();
        Profile p1 = new Profile("Shihab Bhuiyan");
-<<<<<<< HEAD
        AddQuestions add =new AddQuestions();
+       UpQuestions up =new UpQuestions();
+       deleteQ del = new deleteQ();
        //s2.setVisible(true);
       //s1.setVisible(true);
       //h1.setVisible(true);
       //s1.setVisible(true);
       //a1.setVisible(true);
-=======
        QuizPage q1 = new QuizPage();
        Subject sub1 = new Subject();
        Result r1 = new Result();
@@ -27,20 +27,22 @@ public class Main{
       //h1.setVisible(true);
       //  s1.setVisible(true);
       // a1.setVisible(true);
->>>>>>> 29d2235c4a5c7d4c9cdb91a5da1f6efffd465bb9
       // s1.setVisible(true);
       //h1.setVisible(true);
       //  s1.setVisible(true);
       //a1.setVisible(true);
-<<<<<<< HEAD
-      //p1.setVisible(true);
-      add.setVisible(true);
 
-=======
+      //p1.setVisible(true);
+      //add.setVisible(true);
+      //up.setVisible(true);
+      del.setVisible(true);
+      
+
+
       // p1.setVisible(true);
-      // q1.setVisible(true);
-      // sub1.setVisible(true);
-      r1.setVisible(true);
->>>>>>> 29d2235c4a5c7d4c9cdb91a5da1f6efffd465bb9
+       //q1.setVisible(true);
+      //sub1.setVisible(true);
+      //r1.setVisible(true);
+
     }
 }
